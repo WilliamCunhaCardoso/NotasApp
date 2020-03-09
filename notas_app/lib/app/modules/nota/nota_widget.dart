939bf94bc630a98479feaa6a51f38b5137c81567
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-09 16:43:07 -0300

    criado os campos para implementar vars

--- notas_app/lib/app/modules/nota/nota_widget.dart
+++ notas_app/lib/app/modules/nota/nota_widget.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class NotaWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''), // todo: set title var
+      ),
+      body: Container(
+        child: Text(''),
+      ), // todo: implement body text note
+      bottomSheet: Text(''), // todo: set ultima edicao
+    );
   }
 }

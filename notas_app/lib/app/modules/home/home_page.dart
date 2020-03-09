91b52e384c7117f161f1c8817845eb1b8da822b1
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-09 16:43:30 -0300

    implementado parcialmente push named para rotear

--- notas_app/lib/app/modules/home/home_page.dart
+++ notas_app/lib/app/modules/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -25,14 +24,9 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
           // todo: Lista vertical com as notas criadas
         ],
       ),
-      bottomSheet: SolidBottomSheet(
-        headerBar: Icon(Icons.add),
-        body: Expanded(
-          child: Center(
-            // todo: pagina de criação de nota
-            child: Text('Hello World'),
-          ),
-        ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Modular.to.pushNamed(''), // todo implement push named
       ),
     );
   }

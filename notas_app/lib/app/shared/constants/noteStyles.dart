b1a1150b604e25ef94eb874747beaa57c98cde3c
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-10 15:44:12 -0300

    estilos criados

---
+++ notas_app/lib/app/shared/constants/noteStyles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+var titleStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+
+var titleDecoration = InputDecoration(
+  hintText: 'Digite o título',
+);
+
+var bodyStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+);
+
+var dataStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w200,
+  fontSize: 12,
+);

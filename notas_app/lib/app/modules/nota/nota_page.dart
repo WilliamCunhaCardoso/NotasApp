a75d007475169c45bf6b3f810aeea586844c68f8
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-10 10:50:41 -0300

    listagem adicionada

--- notas_app/lib/app/modules/nota/nota_page.dart
+++ notas_app/lib/app/modules/nota/nota_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:notas_app/app/data/db/mydatabase.dart';
+
+import 'nota_controller.dart';
 
 class NotaPage extends StatefulWidget {
   final String title;
@@ -9,16 +14,35 @@ class NotaPage extends StatefulWidget {
 }
 
 class _NotaPageState extends State<NotaPage> {
+  var notaController = Modular.get<NotaController>();
+  var db = MyDatabase.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''), // todo: set title var
+    return Observer(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(notaController.titulo), // todo: set title var
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => db.notaDAO.addNota(
+                      Nota(
+                        conteudo: notaController.texto,
+                        id: null,
+                        titulo: notaController.titulo,
+                        ultimaAlteracao: notaController.ultimaAlteracao,
+                      ),
+                    ),
+                child: Text('Salvar'))
+          ],
+        ),
+        body: Container(
+          child: Text(notaController.texto),
+        ), // todo: implement body text note
+        bottomSheet: Text(
+          notaController.ultimaAlteracao.toString(),
+        ), // todo: set ultima edicao
       ),
-      body: Container(
-        child: Text(''),
-      ), // todo: implement body text note
-      bottomSheet: Text(''), // todo: set ultima edicao
     );
   }
 }

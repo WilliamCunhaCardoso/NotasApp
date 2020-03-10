c98f466829f2d5fd763ba6e3336e95bcd9518dff
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-10 15:45:22 -0300

    half implemented txtedtctrl

--- notas_app/lib/app/modules/nota/nota_page.dart
+++ notas_app/lib/app/modules/nota/nota_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:notas_app/app/data/db/mydatabase.dart';
+import 'package:notas_app/app/shared/constants/noteStyles.dart';
 
 import 'nota_controller.dart';
 
@@ -14,34 +15,57 @@ class NotaPage extends StatefulWidget {
 }
 
 class _NotaPageState extends State<NotaPage> {
+  //* Instanciadores
   var notaController = Modular.get<NotaController>();
   var db = MyDatabase.instance;
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Observer(
-      builder: (BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text(notaController.titulo), // todo: set title var
-          actions: <Widget>[
-            FlatButton(
-                onPressed: () => db.notaDAO.addNota(
-                      Nota(
-                        conteudo: notaController.texto,
-                        id: null,
-                        titulo: notaController.titulo,
-                        ultimaAlteracao: notaController.ultimaAlteracao,
-                      ),
-                    ),
-                child: Text('Salvar'))
-          ],
+      builder: (BuildContext context) => Form(
+        key: formKey,
+        child: Scaffold(
+          appBar: AppBar(
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Salvar'),
+                onPressed: () {
+                  if (formKey.currentState.validate()) {
+                    print('submetido '+notaController.tituloController.text);
+                  }
+                },
+                // onPressed: () =>
+                // db.notaDAO.addNota(
+                //   Nota(
+                //     conteudo: notaController.texto,
+                //     id: null,
+                //     titulo: notaController.tituloController.text,
+                //     ultimaAlteracao: notaController.ultimaAlteracao,
+                //   ),
+                // ),
+              )
+            ],
+          ),
+          body: Container(
+            child: TextFormField(
+              controller: notaController.tituloController,
+              style: titleStyle,
+              decoration: titleDecoration,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onFieldSubmitted: (value) => notaController.tituloController.text = value,
+            ), // todo: implement body text note
+          ),
+          bottomSheet: Text(
+            notaController.ultimaAlteracao.toString(),
+            style: dataStyle,
+          ),
         ),
-        body: Container(
-          child: Text(notaController.texto),
-        ), // todo: implement body text note
-        bottomSheet: Text(
-          notaController.ultimaAlteracao.toString(),
-        ), // todo: set ultima edicao
       ),
     );
   }

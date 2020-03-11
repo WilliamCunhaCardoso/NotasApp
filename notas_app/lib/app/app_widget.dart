7a03c7315ce35db97d996c8ad1516c12bf0ee45c
William Cunha Cardoso <willcunhacardoso@gmail.com>
2020-03-11 16:46:48 -0300

    color changes

--- notas_app/lib/app/app_widget.dart
+++ notas_app/lib/app/app_widget.dart
@@ -8,7 +8,8 @@ class AppWidget extends StatelessWidget {
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xffccbbff),
+        accentColor: Color(0xffccbbff),
       ),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
